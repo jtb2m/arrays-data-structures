0040f7fb119714123e8c58f871107dbe8d18d105
JSITx <javastackittrainings@gmail.com>
2024-11-09 20:09:30 +0530

    First Commit

---
+++ 1DArray.java
@@ -0,0 +1,24 @@
+public class 1DArray
+{
+	public static void main(String[] args) {
+	    // Initialize Array
+		  int[] array = new int[10];
+		
+		  // Declaration of Arrays
+		  array[0] = 10;
+		  array[1] = 20;
+		  array[2] = 30;
+		  array[3] = 40;
+		  array[4] = 50;
+		  array[5] = 60;
+		  array[6] = 70;
+		  array[7] = 80;
+		  array[8] = 90;
+		  array[9] = 100;
+		
+		  // Printing all Elements of Array
+		  for(int i = 0; i < array.length; i++){
+		      System.out.print(array[i] + "\t");
+		  }
+	  }
+}
